2d9737a82a4386ef14e7c9aaa21c30aa767fccc0
Mehdi <mehdijmlkh1382@gmail.com>
2024-05-05 10:56:04 +0330

    refactor: add getter tag for main declaration

--- phase2/src/main/ast/nodes/declaration/MainDeclaration.java
+++ phase2/src/main/ast/nodes/declaration/MainDeclaration.java
@@ -1,14 +1,17 @@
 package main.ast.nodes.declaration;
 
+import lombok.Getter;
+import lombok.Setter;
 import main.ast.nodes.statement.Statement;
 import main.visitor.IVisitor;
 
 import java.util.ArrayList;
 
+@Getter
+@Setter
 public class MainDeclaration extends Declaration{
     private ArrayList<Statement> body = new ArrayList<>();
-    public ArrayList<Statement> getBody() {return this.body;}
-    public void setBody(ArrayList<Statement>body){this.body = body;}
+
     public void addBody(Statement stmt){this.body.add(stmt);}
     @Override
     public String toString(){return "MainDeclaration";}
