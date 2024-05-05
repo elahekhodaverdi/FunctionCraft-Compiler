46c14a899425e091194df73f3849fa383a0f005c
Mehdi <mehdijmlkh1382@gmail.com>
2024-05-05 10:58:03 +0330

    refactor: add getter tag for put statement

--- phase2/src/main/ast/nodes/statement/PutStatement.java
+++ phase2/src/main/ast/nodes/statement/PutStatement.java
@@ -1,13 +1,16 @@
 package main.ast.nodes.statement;
 
+import lombok.Getter;
+import lombok.Setter;
 import main.ast.nodes.expression.Expression;
 import main.visitor.IVisitor;
 
+@Getter
+@Setter
 public class PutStatement extends Statement{
     private Expression expression;
     public PutStatement(Expression expression){this.expression = expression;}
-    public Expression getExpression(){return this.expression;}
-    public void setExpression(Expression expression){this.expression = expression;}
+
     @Override
     public String toString(){return "PutStatement";}
     @Override
