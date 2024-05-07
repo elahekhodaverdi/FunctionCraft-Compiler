d178e93f598ef6269b9ee61d6fd17746cc8d3fa6
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-05-07 16:18:59 +0330

    feat: add pre symbole table

--- phase2/src/main/symbolTable/SymbolTable.java
+++ phase2/src/main/symbolTable/SymbolTable.java
@@ -13,9 +13,11 @@ import java.util.Map;
 public class SymbolTable {
     public static SymbolTable top;
     public static SymbolTable root;
+    private SymbolTable pre;
     private static Stack<SymbolTable> stack = new Stack<>();
     private Map<String, SymbolTableItem> items;
     public static void push(SymbolTable symbolTable) {
+        symbolTable.pre = top;
         if (top != null)
             stack.push(top);
         top = symbolTable;
@@ -25,6 +27,7 @@ public class SymbolTable {
     }
     public SymbolTable() {
         this.items = new HashMap<>();
+        this.pre = null;
     }
     public void put(SymbolTableItem item) throws ItemAlreadyExists {
         if (items.containsKey(item.getKey()))
@@ -32,11 +35,19 @@ public class SymbolTable {
         items.put(item.getKey(), item);
     }
     public SymbolTableItem getItem(String key) throws ItemNotFound {
-        SymbolTableItem symbolTableItem = this.items.get(key);
-        if( symbolTableItem != null ){
-            return symbolTableItem;
+        SymbolTable currentSymbolTable = this;
+        SymbolTableItem symbolTableItem = null;
+
+        while (symbolTableItem == null && currentSymbolTable != null) {
+            symbolTableItem = currentSymbolTable.items.get(key);
+            currentSymbolTable = currentSymbolTable.pre;
         }
-        throw new ItemNotFound();
+
+        if( symbolTableItem == null )
+            throw new ItemNotFound();
+
+        return symbolTableItem;
+    
     }
 
     public FunctionItem getFunctionItem(String key) throws ItemNotFound {
