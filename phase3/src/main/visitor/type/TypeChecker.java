c4fb27dce6fdabbc025a67b13b6e7e4535068b98
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-05-21 12:09:11 +0330

    refactor: rename listType to type

--- phase3/src/main/visitor/type/TypeChecker.java
+++ phase3/src/main/visitor/type/TypeChecker.java
@@ -220,17 +220,17 @@ public class TypeChecker extends Visitor<Type> {
     @Override
     public Type visit(ListValue listValue){
         // TODO:visit listValue
-        Type listType = new NoType();
+        Type type = new NoType();
         for(Expression expression: listValue.getElements()) {
             Type currElementType  = expression.accept(this);
-            if (listType instanceof NoType)
-                listType = currElementType;
-            else if(!listType.sameType(currElementType)){
+            if (type instanceof NoType)
+                type = currElementType;
+            else if(!type.sameType(currElementType)){
                 typeErrors.add(new ListElementsTypesMisMatch(listValue.getLine()));
                 return new NoType();
             }
         }
-        return listType;
+        return new ListType(type);
     }
     @Override
     public Type visit(FunctionPointer functionPointer){
