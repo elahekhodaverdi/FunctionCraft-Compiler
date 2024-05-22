07f107043285892ed5c7c7c4a89198ec5b892856
Mehdi <mehdijmlkh1382@gmail.com>
2024-05-22 12:33:28 +0330

    Revert "feat: add before and after function for type checker test"
    This reverts commit c1d84d9a10c49685fc624248dcf4d34bb6b85d1a.

--- phase3/src/test/visitor/type/TypeCheckerTest.java
+++ phase3/src/test/visitor/type/TypeCheckerTest.java
@@ -1,42 +1,11 @@
 package test.visitor.type;
 
-import main.FunctionCraft;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.IOException;
-
-import static org.junit.Assert.assertEquals;
 
 public class TypeCheckerTest {
 
-    private int testCaseNumber;
-    private ByteArrayOutputStream outputStream;
+    @Test
+    public void test() {
 
-    @Before
-    public void setUp() {
-        outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-    }
-
-    @After
-    public void assertEqualErrors() throws IOException {
-
-        String[] args = new String[]{"samples/input/sample" + testCaseNumber + ".fl"};
-        String expectedOutputPath = "samples/output/out" + testCaseNumber + ".txt";
-
-        FunctionCraft.main(args);
-
-        Path path = Paths.get(expectedOutputPath);
-        String actualOutput = outputStream.toString().trim();
-        String expectedOutput = Files.readString(path).trim();
-
-        assertEquals(expectedOutput, actualOutput);
     }
 }
