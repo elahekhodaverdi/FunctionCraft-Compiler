145b86ba0586f015c5d07a0c651194e5ba859ea8
Mehdi <mehdijmlkh1382@gmail.com>
2024-05-23 11:37:15 +0330

    style: remove extra paranteses

--- phase3/src/main/ast/type/Type.java
+++ phase3/src/main/ast/type/Type.java
@@ -4,7 +4,7 @@ package main.ast.type;
 
 public abstract class Type {
     public boolean sameType(Type other){
-        return (this.getClass().equals(other.getClass())) || ((this instanceof NoType) || (other instanceof NoType));
+        return this.getClass().equals(other.getClass()) || (this instanceof NoType) || (other instanceof NoType);
     }
 
     public boolean isNumericType() {return false;}
